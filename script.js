a920e2b988162f45f0da0a6e73770679c76b6855
Seingblu1207 <oveorozco@gmail.com>
2023-01-29 01:29:34 -0500

    Botones funcionales, cambio de paleta de colores y contenido principal

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 let search = document.querySelector('.search-box');
 
-document.querySelector('#search-icon').onclick  =()=>{
+document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 }
+
+const HomeBtn = document.querySelector('#HomeBtn');
+const ContactosBtn = document.querySelector('#ContactosBtn');
+const ServiciosBtn = document.querySelector('#ServiciosBtn');
+const BlogBtn = document.querySelector('#BlogBtn');
+const AcercaBtn = document.querySelector('#AcercaBtn');
+const ClientesBtn = document.querySelector('#ClientesBtn');
+
+
+const homeSection = document.querySelector('#Home');
+const aboutSection = document.querySelector('#About');
+const ServicesSection = document.querySelector('#Services');
+// const homeSection = document.querySelector('#Home');
+// const homeSection = document.querySelector('#Home');
+// const homeSection = document.querySelector('#Home');
+
+
+HomeBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: homeSection.offsetTop,
+        behavior: 'smooth'
+    });
+});
+
+AcercaBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: aboutSection.offsetTop,
+        behavior: 'smooth'
+    });
+});
+
+productsSection.addEventListener('click', () => {
+    window.scrollTo({
+        top: ServicesSection.offsetTop,
+        behavior: 'smooth'
+    });
+});
