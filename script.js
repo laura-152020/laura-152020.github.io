f91c1ddfe29c37054555b536e05787d453fbeb8c
Seingblu1207 <oveorozco@gmail.com>
2023-01-29 03:24:20 -0500

    Optimizacion y documentacion de JS y cambios menores

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
-let search = document.querySelector('.search-box');
+// Select all the elements at the start 
+let search = document.querySelector('.search-box'); 
+const HomeBtn = document.querySelector('#HomeBtn'); 
+const ContactosBtn = document.querySelector('#ContactosBtn'); 
+const ServiciosBtn = document.querySelector('#ServiciosBtn'); 
+const BlogBtn = document.querySelector('#BlogBtn'); 
+const AcercaBtn = document.querySelector('#AcercaBtn');  
+const ClientesBtn = document.querySelector('#ClientesBtn'); 
 
-document.querySelector('#search-icon').onclick = () => {
-    search.classList.toggle('active');
-}
+const homeSection = document.querySelector('#Home'); 
+const aboutSection = document.querySelector('#About'); 
+const ServicesSection = document.querySelector('#Services'); 
+const blogSection = document.querySelector('#Home'); 
+const AcercaSection = document.querySelector('Home'); 
+const ClientesSection = document.querySelector('#Home'); 
 
-const HomeBtn = document.querySelector('#HomeBtn');
-const ContactosBtn = document.querySelector('#ContactosBtn');
-const ServiciosBtn = document.querySelector('#ServiciosBtn');
-const BlogBtn = document.querySelector('#BlogBtn');
-const AcercaBtn = document.querySelector('#AcercaBtn');
-const ClientesBtn = document.querySelector('#ClientesBtn');
+// Attach click handlers to all the elements 
+// adds a toggle class of active to the search element on click of #HomeBtn 
+HomeBtn.onclick = () => { 
+  search.classList.toggle('active'); 
+  window.scrollTo({ 
+    top: homeSection.offsetTop, 
+    behavior: 'smooth' 
+  }); 
+}; 
 
+// Smooth scroll to aboutSection on click of #ContactosBtn 
+ContactosBtn.onclick = () => { 
+  window.scrollTo({ 
+    top: aboutSection.offsetTop, 
+    behavior: 'smooth' 
+  }); 
+}; 
 
-const homeSection = document.querySelector('#Home');
-const aboutSection = document.querySelector('#About');
-const ServicesSection = document.querySelector('#Services');
-// const homeSection = document.querySelector('#Home');
-// const homeSection = document.querySelector('#Home');
-// const homeSection = document.querySelector('#Home');
+// Smooth scroll to ServicesSection on click of #ServiciosBtn 
+ServiciosBtn.onclick = () => { 
+  window.scrollTo({ 
+    top: ServicesSection.offsetTop, 
+    behavior: 'smooth' 
+  }); 
+}; 
 
+//Smooth scroll to blogSection on click of #BlogBtn 
+BlogBtn.onclick = () => { 
+  window.scrollTo({ 
+    top: blogSection.offsetTop, 
+    behavior: 'smooth' 
+  }); 
+}; 
 
-HomeBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: homeSection.offsetTop,
-        behavior: 'smooth'
-    });
-});
+// Smooth scroll to AcercaSection on click of #AcercaBtn 
+AcercaBtn.onclick = () => { 
+  window.scrollTo({ 
+    top: AcercaSection.offsetTop, 
+    behavior: 'smooth' 
+  }); 
+}; 
 
-AcercaBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: aboutSection.offsetTop,
-        behavior: 'smooth'
-    });
-});
-
-productsSection.addEventListener('click', () => {
-    window.scrollTo({
-        top: ServicesSection.offsetTop,
-        behavior: 'smooth'
-    });
-});
+//Smooth scroll to ClientesSection on click of #ClientesBtn 
+ClientesBtn.onclick = () =>{ 
+  window.scrollTo({ 
+    top: ClientesSection.offsetTop, 
+    behavior: 'smooth' 
+  }); 
+}; 
